69b1ab6e9737da1af1f0d2d8f34df757a2ee9c4b
zhendrix <zhendrix@my.wctc.edu>
2023-10-24 14:41:56 -0500

    Added Search with Results

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿﻿using NLog;
+
+// See https://aka.ms/new-console-template for more information
+string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+// create instance of Logger
+var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+logger.Info("Program started");
+
+string movieFilePath = Directory.GetCurrentDirectory() + "\\movies.csv";
+MovieFile movieFile = new MovieFile(movieFilePath);
+string choice = "";
+do
+{
+    // display choices to user
+    Console.WriteLine("1) Add Movie");
+    Console.WriteLine("2) Display All Movies");
+    Console.WriteLine("3) Find Movie");
+    Console.WriteLine("Enter to quit");
+    // input selection
+    choice = Console.ReadLine();
+    logger.Info("User choice: {Choice}", choice);
+
+    if (choice == "1")
+    {
+        // Add movie
+        Movie movie = new Movie();
+        // ask user to input movie title
+        Console.WriteLine("Enter movie title");
+        // input title
+        movie.title = Console.ReadLine();
+        // verify title is unique
+        if (movieFile.isUniqueTitle(movie.title))
+        {
+            Console.WriteLine("Movie title is unique\n");
+            // input genres
+            string input;
+            do
+            {
+                // ask user to enter genre
+                Console.WriteLine("Enter genre (or done to quit)");
+                // input genre
+                input = Console.ReadLine();
+                // if user enters "done"
+                // or does not enter a genre do not add it to list
+                if (input != "done" && input.Length > 0)
+                {
+                    movie.genres.Add(input);
+                }
+            } while (input != "done");
+            // specify if no genres are entered
+            if (movie.genres.Count == 0)
+            {
+                movie.genres.Add("(no genres listed)");
+            }
+        }
+    }
+    else if (choice == "2")
+    {
+        // Display All Movies
+        foreach (Movie m in movieFile.Movies)
+        {
+            Console.WriteLine(m.Display());
+        }
+    }
+    
+        
+
+        
+        else if (choice == "3")
+    {
+        Console.WriteLine("Enter title to search for:");
+        string searchTitle = Console.ReadLine().Trim();
+
+        var matchingMovies = movieFile.SearchMoviesByTitle(searchTitle);
+
+        if (matchingMovies.Count > 0)
+        {
+            Console.WriteLine($"{matchingMovies.Count} matches found:");
+            foreach (var movie in matchingMovies)
+            {
+                Console.WriteLine(movie.Display());
+            }
+        }
+        else
+        {
+            Console.WriteLine("No movies found with that title.");
+        }
+    }
+} while (choice == "1" || choice == "2" || choice == "3");
+
+logger.Info("Program ended");
+
+
+
+
+
+
